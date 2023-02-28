bf6bb137485d478296539af79e807ef35cc9e395
Marco Dung <6645012+mdger@users.noreply.github.com>
2023-02-28 20:16:33 +0100

    asd

--- src/renderer/src/Editor.vue
+++ src/renderer/src/Editor.vue
@@ -62,6 +62,18 @@ function render(): void {
 
 const canvas = ref<HTMLDivElement | null>(null)
 
+
+const currentMenuIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]): void => {
+  console.log(key, keyPath)
+}
+
+const handleOpen = (key: string, keyPath: string[]): void => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]): void => {
+  console.log(key, keyPath)
+}
 onMounted(() => {
   init()
   render()
@@ -70,9 +82,54 @@ onMounted(() => {
 </script>
 
 <template>
+  <div class="menu">
+    <el-header class="header">
+      <!-- TOP NAVIGATION -->
+      <el-menu :default-active="currentMenuIndex" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-sub-menu index="2">
+          <template #title>Workspace</template>
+          <el-menu-item index="2-1">item one</el-menu-item>
+          <el-menu-item index="2-2">item two</el-menu-item>
+          <el-menu-item index="2-3">item three</el-menu-item>
+          <el-sub-menu index="2-4">
+            <template #title>item four</template>
+            <el-menu-item index="2-4-1">item one</el-menu-item>
+            <el-menu-item index="2-4-2">item two</el-menu-item>
+            <el-menu-item index="2-4-3">item three</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="3" disabled>Info</el-menu-item>
+        <el-menu-item index="4">Orders</el-menu-item>
+        <div class="flex-grow" />
+        <el-menu-item>The Tool</el-menu-item>
+      </el-menu>
+      <div class="h-6" />
+    </el-header>
+  </div>
+
   <div ref="canvas" class="renderer"></div>
 </template>
 
 <style lang="less">
 @import './assets/css/styles.less';
+
+.menu {
+  position: absolute;
+  // width: 100vw;
+  // height: 100vh;
+}
+
+.header {
+  padding: 0;
+  width: 100vw;
+}
+
+.flex-grow {
+  flex-grow: 1;
+}
+
+.sidebar {
+  overflow-y: scroll;
+}
 </style>
